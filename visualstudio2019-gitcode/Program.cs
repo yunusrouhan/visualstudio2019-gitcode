279014895c9d0f6af585e21764cd39978ada5cb4
rouha <rouha@DESKTOP-MU91PTE>
2019-09-09 19:24:18 -0400

    commiting a change

---
+++ visualstudio2019-gitcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace visualstudio2019_gitcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
